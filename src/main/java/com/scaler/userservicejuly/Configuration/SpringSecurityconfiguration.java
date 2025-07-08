74e449f4d429d519464b97c2c2054f7d383219ac
Shashank Basani <shashankbasani44@gmail.com>
2025-07-08 13:25:42 +0530

    added SpringSecurityconfiguration.java

--- src/main/java/com/scaler/userservicejuly/Configuration/SpringSecurityconfiguration.java
+++ src/main/java/com/scaler/userservicejuly/Configuration/SpringSecurityconfiguration.java
@@ -1,23 +1,23 @@
-package com.scaler.userservicejuly.Configuration;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-public class SpringSecurityconfiguration {
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws  Exception{
-        http.authorizeHttpRequests((request)->{
-            try {
-                request.anyRequest().permitAll()
-                        .and().cors().disable()
-                        .csrf().disable();
-            }catch (Exception e){
-                throw new RuntimeException(e);
-            }
-        });
-        return http.build();
-    }
-}
+//package com.scaler.userservicejuly.Configuration;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@Configuration
+//public class SpringSecurityconfiguration {
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws  Exception{
+//        http.authorizeHttpRequests((request)->{
+//            try {
+//                request.anyRequest().permitAll()
+//                        .and().cors().disable()
+//                        .csrf().disable();
+//            }catch (Exception e){
+//                throw new RuntimeException(e);
+//            }
+//        });
+//        return http.build();
+//    }
+//}
